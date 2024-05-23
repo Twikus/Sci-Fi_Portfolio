fc062da0c1e901e5afc55ec5b5362c698826138d
Axel D <axel@spliiit.com>
2024-05-23 23:42:15 +0200

    feat/home-screen : add gradient

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,32 @@ import { RouterView } from 'vue-router'
   <body>
     <RouterView />
     <img src="@/assets/img/layout.png" alt="layout" class="layout" />
+    <div class="black-opacity" ></div>
+    <video autoplay loop muted class="video">
+      <source src="@/assets/img/background.mp4" type="video/mp4" />
+    </video>
   </body>
 </template>
+
+<style scoped>
+  .black-opacity {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    opacity: 0.7;
+    z-index: -999;
+  }
+  .video {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -1000;
+  }
+</style>
