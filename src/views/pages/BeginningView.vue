c36a19b483053bb360d0c30a2e908f8799e5f671
Axel D <axel@spliiit.com>
2024-05-26 18:33:23 +0200

    feat/beginning-view : add beginning view

--- src/views/pages/BeginningView.vue
+++ src/views/pages/BeginningView.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        begin
+    <div class="beginning-container">
+        <h2>NAVIGATING THE NEON CORRIDORS OF INTERSTELLAR DATA STREAMS, BRINGING INNOVATION AND EXCITEMENT TO USERS IN EVERY CORNER OF THE GALAXY.</h2>
+        <p>ARTWORK GENERATED WITH MIDJOURNEY</p>
+        <img src="/src/assets/img/animal.png" alt="whale">
     </div>
 </template>
+
+<style lang="scss">
+.beginning-container {
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h2 {
+        font-size: 18px;
+        width: 600px;
+        text-align: center;
+    }
+
+    p {
+        font-size: 12px;
+        margin-top: 10px;
+        color: var(--grey-primary);
+    }
+
+    img {
+        width: 100%;
+        opacity: 0.7;
+    }
+}
+</style>
